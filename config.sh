1db697d7e79f0fc10a77410011e59e7d6856a36f
Flavio Cruz <flaviocruz@gmail.com>
2021-01-22 19:29:37 -0800

    Use nproc to set PROCS for better parallelism out of the box.

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 export HOST_MACHINE=x86_64-linux-gnu
 
 # Change how many CPUs you want to use.
-export PROCS=1
+export PROCS=$(nproc)
 
 # Set this to your ccache path if using ccache.
 export CCACHE_DIRECTORY="/usr/lib/ccache"
