ed9779a838c5a4991adc4c3b2d5bbd1542e424c6
Flavio Cruz <flaviocruz@gmail.com>
2022-12-20 21:18:19 -0500

    Add script to compile a kernel only

---
+++ bootstrap-kernel.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+. ./vars.sh
+. ./download-funcs.sh
+. ./common.sh
+. ./bootstrap-funcs.sh
+
+print_info "Root is $ROOT"
+print_info "Cross-compiling on $HOST to $TARGET"
+
+setup_directories
+
+install_gnumach() {
+   cd "$GNUMACH_SRC" &&
+   autoreconf -i &&
+   cd .. &&
+   rm -rf $GNUMACH_SRC.second_obj &&
+   mkdir -p "$GNUMACH_SRC".second_obj &&
+   cd "$GNUMACH_SRC".second_obj &&
+   CFLAGS="-ffreestanding -nostdlib" ../$GNUMACH_SRC/configure \
+      --host="$TARGET" \
+      --build="$HOST" \
+      --exec-prefix=/tmp/throwitaway \
+      --enable-kdb \
+      --enable-kmsg \
+      --prefix="$SYS_ROOT" &&
+   make -j$PROCS gnumach.gz gnumach gnumach.msgids &&
+   make -j$PROCS install &&
+   mkdir -p $SYSTEM/boot &&
+   cp gnumach.gz $SYSTEM/boot/ &&
+   cd -
+}
+
+set_vars() {
+   export CC="${TARGET}-gcc"
+   export CXX="${TARGET}-g++"
+   export AR="${ROOT}/bin/${TARGET}-ar"
+   export AS="${ROOT}/bin/${TARGET}-as"
+   export RANLIB="${ROOT}/bin/${TARGET}-ranlib"
+   export LD="${ROOT}/bin/${TARGET}-ld"
+   export STRIP="${ROOT}/bin/${TARGET}-strip"
+   export MIG="${ROOT}/bin/${TARGET}-mig"
+}
+
+ cd src &&
+   compile_binutils &&
+   compile_gcc &&
+   compile_pkgconfiglite &&
+   install_gnumach_headers &&
+   install_gnumig &&
+   install_hurd_headers &&
+   set_vars &&
+   install_gnumach &&
+   print_info "bootstrap.sh finished successfully" &&
+   exit 0
