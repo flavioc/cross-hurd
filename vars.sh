3a5f6dc983cf8ccb278e9573e1dc967ebe001f0f
Flavio Cruz <flaviocruz@gmail.com>
2023-05-01 23:07:28 -0400

    Move package versions to package-versions.sh

--- vars.sh
+++ vars.sh
@@ -18,35 +18,7 @@ if [ ! -z "$CCACHE_DIRECTORY" ]; then
    export PATH=$CCACHE_DIRECTORY:$PATH
 fi
 
-# Package versions.
-BINUTILS_VERSION=2.40
-GCC_VERSION=13.1.0
-FLEX_VERSION=2.6.4
-ZLIB_VERSION=1.2.13
-BASH_VERSION=5.2
-COREUTILS_VERSION=9.1
-# e2fsprogs 1.45.4 is failing due to something with sys/mount.h in configure
-E2FSPROGS_VERSION=1.44.1
-PKGCONFIGLITE_VERSION=0.28-1
-LIBUUID_VERSION=1.0.3
-UTIL_LINUX_VERSION=2.38
-GRUB_VERSION=2.06
-# Shadow 4.7 is failing due to sys/prctl.h missing
-SHADOW_VERSION=4.6
-SED_VERSION=4.9
-GMP_VERSION=6.2.1
-MPFR_VERSION=4.2.0
-MPC_VERSION=1.3.1
-NCURSES_VERSION=6.4
-# 8.1: checking what tgetent() returns for an unknown terminal... configure: error: failed to compile test program.
-VIM_BASE_VERSION=90
-VIM_VERSION=9.0
-GPG_ERROR_VERSION=1.46
-GCRYPT_VERSION=1.10.1
-MAKE_VERSION=4.3
-GREP_VERSION=3.10
-GAWK_VERSION=5.2.1
-# Mach, Hurd and Glibc are all taken from the Git repository.
+. ./package-versions.sh
 
 BINUTILS_SRC=binutils-"$BINUTILS_VERSION"
 BINUTILS_PKG="${BINUTILS_SRC}.tar.bz2"
