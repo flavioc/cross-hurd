186cafffd18da1e7c39e584865cf1b083a42cb5e
Flavio Cruz <flaviocruz@gmail.com>
2023-05-12 20:52:35 -0400

    Allow setting PROCS to control build parallelism.

--- config.sh
+++ config.sh
@@ -2,7 +2,9 @@
 export HOST_MACHINE=x86_64-linux-gnu
 
 # Change how many CPUs you want to use.
-export PROCS=$(nproc)
+if [ -z "$PROCS" ]; then
+	export PROCS=$(nproc)
+fi
 
 # Set this to your ccache path if using ccache.
 export CCACHE_DIRECTORY="/usr/lib/ccache"
